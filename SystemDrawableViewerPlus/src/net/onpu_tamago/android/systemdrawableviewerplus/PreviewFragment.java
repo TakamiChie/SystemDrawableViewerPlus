824b40c5a9c8f9330638b25c11befb793cfe5b8d
TakamiChie <chie@onpu-tamago.net>
2013-08-19 16:19:23 +0900

    アイコンサイズを表示する機能を追加

--- SystemDrawableViewerPlus/src/net/onpu_tamago/android/systemdrawableviewerplus/PreviewFragment.java
+++ SystemDrawableViewerPlus/src/net/onpu_tamago/android/systemdrawableviewerplus/PreviewFragment.java
@@ -1,7 +1,9 @@
 package net.onpu_tamago.android.systemdrawableviewerplus;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,7 @@ import com.androidquery.AQuery;
 
 public class PreviewFragment extends Fragment {
 
+	private static final String TAG = "[Preview]SystemDrawableViewerPlus";
 	private View mView;
 
 	@Override
@@ -28,7 +31,15 @@ public class PreviewFragment extends Fragment {
 		Bundle bundle = getArguments();
 		if (bundle != null) {
 			int iconid = bundle.getInt(MainFragment.EXTRA_ICONID, 0);
+			Drawable icon = getResources().getDrawable(iconid);
+			Log.d(TAG, bundle.getString(MainFragment.EXTRA_ICONNAME));
+			Log.d(TAG, "MinWidth:" + icon.getMinimumWidth());
+			Log.d(TAG, "MinHeight:" + icon.getMinimumHeight());
+			Log.d(TAG, "IntrinsicWidth:" + icon.getIntrinsicWidth());
+			Log.d(TAG, "IntrinsicHeight:" + icon.getIntrinsicHeight());
 			AQuery $ = new AQuery(mView);
+			$.id(R.id.out_iconsize).text(R.string.wording_size,
+					icon.getMinimumWidth(), icon.getMinimumHeight());
 			$.id(R.id.out_preview1).background(iconid);
 			$.id(R.id.out_preview2).background(iconid);
 			$.id(R.id.out_preview3).background(iconid);
